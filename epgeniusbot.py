c0e909279b0348bd40ba6a6e4ff67823bbdd32bf
greenspeedracer <kozelr@gmail.com>
2025-10-01 21:36:30 -0400

    fix imports

--- epgeniusbot.py
+++ epgeniusbot.py
@@ -3,6 +3,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from discord.ui import View, Select
+from discord.app_commands import MissingAnyRole
 import re
 import os
 import sys
